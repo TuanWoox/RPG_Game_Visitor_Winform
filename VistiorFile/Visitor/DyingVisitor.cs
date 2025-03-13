27cb70ad6f92d2c9dc901c6406dd34b729ea876f
TuanWoox <z1venttt@gmail.com>
2025-03-13 18:37:02 +0700

    Add utils so that code does not repeat itself

--- VistiorFile/Visitor/DyingVisitor.cs
+++ VistiorFile/Visitor/DyingVisitor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
+using winform_charts_visitor.utils;
 using winform_charts_visitor.VistiorFile.Element;
 
 namespace winform_charts_visitor.VistiorFile.Visitor
@@ -11,76 +12,23 @@ namespace winform_charts_visitor.VistiorFile.Visitor
             if (valky == null || valky.GetPictureBox() == null)
                 return;
 
-            // Change the image to attack animation
-            valky.GetPictureBox().Image = Properties.Resources.valkyrie_dying;
-
-            // Use System.Windows.Forms.Timer to reset image after animation
-            Timer resetTimer = new Timer
-            {
-                Interval = 890 // Duration of the attack animation in milliseconds
-            };
-
-            resetTimer.Tick += (s, ev) =>
-            {
-                resetTimer.Stop();
-                resetTimer.Dispose();
-
-                // Reset the image to the standing state
-                valky.GetPictureBox().Image = Properties.Resources.valkyrie_idle;
-            };
-
-            resetTimer.Start();
+            Utils.ChangeImage(Properties.Resources.valkyrie_dying, Properties.Resources.valkyrie_idle, valky.GetPictureBox());
         }
 
-        public void Visit(Golem golem)  // <-- Moved inside AttackVisitor class
+        public void Visit(Golem golem)
         {
             if (golem == null || golem.GetPictureBox() == null)
                 return;
 
-            // Change the image to attack animation
-            golem.GetPictureBox().Image = Properties.Resources.golem_dying;
-
-            // Use System.Windows.Forms.Timer to reset image after animation
-            Timer resetTimer = new Timer
-            {
-                Interval = 890 // Duration of the attack animation in milliseconds
-            };
-
-            resetTimer.Tick += (s, ev) =>
-            {
-                resetTimer.Stop();
-                resetTimer.Dispose();
-
-                // Reset the image to the standing state
-                golem.GetPictureBox().Image = Properties.Resources.golem_idle;
-            };
-
-            resetTimer.Start();
+            Utils.ChangeImage(Properties.Resources.golem_dying, Properties.Resources.golem_idle, golem.GetPictureBox());
         }
-        public void Visit(Reaper reaper)  // <-- Moved inside AttackVisitor class
+
+        public void Visit(Reaper reaper)
         {
             if (reaper == null || reaper.GetPictureBox() == null)
                 return;
 
-            // Change the image to attack animation
-            reaper.GetPictureBox().Image = Properties.Resources.reaper_dying;
-
-            // Use System.Windows.Forms.Timer to reset image after animation
-            Timer resetTimer = new Timer
-            {
-                Interval = 890 // Duration of the attack animation in milliseconds
-            };
-
-            resetTimer.Tick += (s, ev) =>
-            {
-                resetTimer.Stop();
-                resetTimer.Dispose();
-
-                // Reset the image to the standing state
-                reaper.GetPictureBox().Image = Properties.Resources.reaper_idle;
-            };
-
-            resetTimer.Start();
+            Utils.ChangeImage(Properties.Resources.reaper_dying, Properties.Resources.reaper_idle, reaper.GetPictureBox());
         }
     }
 }
---
+++ utils/Utils.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winform_charts_visitor.utils
+{
+    public static class Utils
+    {
+        public static void ChangeImage(Image action, Image normal, PictureBox characterPictureBox)
+        {
+            if (characterPictureBox != null)
+            {
+
+                // Change the image to attack animation
+                characterPictureBox.Image = action;
+
+                // Use System.Windows.Forms.Timer to reset image after animation
+                Timer resetTimer = new Timer
+                {
+                    Interval = 890 // Duration of the attack animation in milliseconds
+                };
+
+                resetTimer.Tick += (s, ev) =>
+                {
+                    resetTimer.Stop();
+                    resetTimer.Dispose();
+
+                    // Reset the image to the standing state
+                    characterPictureBox.Image = normal;
+                };
+
+                resetTimer.Start();
+            }
+        }
+    }
+}
